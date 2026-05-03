7cc132c225587bc060490da1f6c8fd4a122e7a5e
anmol7240 <anmolshooter7240@gmail.com>
2026-05-03 23:48:54 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,6 +10,8 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -49,16 +51,14 @@ class DataIngestion:
     
         
 if __name__=="__main__":
-    try:
-        obj = DataIngestion()
-        train_path, test_path = obj.initiate_data_ingestion()
+    obj = DataIngestion()
+    train_path, test_path = obj.initiate_data_ingestion()
 
-        logging.info("Data ingestion completed")
+    logging.info("Data ingestion completed")
 
-        data_transformation = DataTransformation()
-        data_transformation.initiate_data_transformation(train_path, test_path)
+    data_transformation = DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_path, test_path)
 
-        logging.info("Data transformation completed")
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
 
-    except Exception as e:
-        raise CustomException(e, sys)
